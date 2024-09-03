d9677cf85a73ea6c398e849d3cc5e1f0b2876162
Tanguturi Venkata Lakshmi Ajay <113990108+ajaytanguturi@users.noreply.github.com>
2024-09-03 18:38:18 +0530

    sqroot.java

---
+++ sqroot.java
@@ -0,0 +1,13 @@
+import java.lang.*;
+import java.util.*;
+class sqroot{
+public static void main(String arg[]){
+int n;
+double m;
+Scanner s=new Scanner(System.in);
+System.out.print("enter Value: ");
+n=s.nextInt();
+m=Math.sqrt(n);
+System.out.println(m);
+}
+}
