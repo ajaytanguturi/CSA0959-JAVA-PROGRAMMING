6a7951210228b88eb3af0c4df30b2c56c69b2faf
Tanguturi Venkata Lakshmi Ajay <113990108+ajaytanguturi@users.noreply.github.com>
2024-09-03 17:49:37 +0530

    fibonaccithread.java

---
+++ fibonaccithread.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class fibo extends Thread{
+    private int value;
+    fibo (int v){
+    this.value=v;
+    }
+    public void run(){
+        int fn=0,sn=1;
+        for(int i=0;i<value;i++){
+            System.out.print(fn+" ");
+            int nn= fn+sn;
+            fn=sn;
+            sn=nn;
+        }
+    }
+}
+public class fibonaccithread {
+    public static void main(String args[]){
+        int n;
+        Scanner s=new Scanner(System.in);
+        System.out.println("enter number:");
+        n=s.nextInt();
+        fibo t=new fibo(n);
+        t.start();
+    }
+}
