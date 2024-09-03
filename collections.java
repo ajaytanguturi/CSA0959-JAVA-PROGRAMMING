0135387c87820fd4cdd491cacfe72ca9e7246264
Tanguturi Venkata Lakshmi Ajay <113990108+ajaytanguturi@users.noreply.github.com>
2024-09-03 17:44:07 +0530

    collections.java

---
+++ collections.java
@@ -0,0 +1,53 @@
+import java.util.*;
+public class collections {
+    public static void main(String[] args) {
+       /*  List l=new LinkedList<>();
+        l.add(5);
+        l.add(7);
+        l.add(9);
+        
+        System.out.println(l);
+      */
+      
+      /*Stack s=new Stack<>();
+      s.push(5);
+      s.push(7);
+      s.push(9);
+      s.push(2);
+      s.pop();
+        System.out.println(s);*/
+
+
+        /*Vector v=new Vector<>();
+        v.add(5);
+        v.add(7);
+        v.add(9);
+        v.remove(1);
+        System.out.println(v);*/
+
+        /*short a=40000;
+        System.out.println(a);*/
+
+        /*String s="Hello World";
+        System.out.println(s.toUpperCase());
+        System.out.println(s.substring(7 ));
+        System.out.println(s.replace("o", "e"));*/
+
+
+        /*long l=10;
+        int i=(int)l;
+        System.out.println(i);*/
+
+        String s="10";
+        int i=Integer.parseInt(s);
+        System.out.println(i);
+
+
+        StringBuffer sb=new StringBuffer("Hello");
+        sb.append("cat");
+        System.out.println(sb);
+        
+        
+    }
+    
+}
